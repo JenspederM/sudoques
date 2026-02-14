285ea8c038425f05c4d786d9f6111edad784436c
Jens Peder Meldgaard <jenspederm@gmail.com>
2026-02-14 15:25:16 +0100

    fix: we use bun not vite
    Signed-off-by: Jens Peder Meldgaard <jenspederm@gmail.com>

--- src/firebase.ts
+++ src/firebase.ts
@@ -11,13 +11,13 @@ import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE_FIREBASE_APP_ID,
-	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+	apiKey: process.env.VITE_FIREBASE_API_KEY,
+	authDomain: "sudoques.firebaseapp.com",
+	projectId: "sudoques",
+	storageBucket: "sudoques.firebasestorage.app",
+	messagingSenderId: "93891219633",
+	appId: "1:93891219633:web:c4983a874faf7ea4695290",
+	measurementId: "G-8Y42TPY9P6",
 };
 
 // Initialize Firebase
