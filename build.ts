1d5a4c16ce22e77b460ea1a87fd850d6ffccdcb3
Jens Peder Meldgaard <jenspederm@gmail.com>
2026-02-14 15:29:57 +0100

    build: env variables should be inlined
    Signed-off-by: Jens Peder Meldgaard <jenspederm@gmail.com>

--- build.ts
+++ build.ts
@@ -152,6 +152,7 @@ const result = await Bun.build({
 	minify: true,
 	target: "browser",
 	sourcemap: "linked",
+	env: "BUN_PUBLIC_*",
 	define: {
 		"process.env.NODE_ENV": JSON.stringify("production"),
 	},
